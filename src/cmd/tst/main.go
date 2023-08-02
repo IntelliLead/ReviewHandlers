9bcb0190173efd1d9ab973bb1c5d18392e3f8657
Shawn <shawnw@vest.fyi>
2023-08-02 11:35:27 +0800

    wip

--- src/cmd/tst/main.go
+++ src/cmd/tst/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "context"
+    "fmt"
     "github.com/IntelliLead/ReviewHandlers/src/pkg/ddbDao"
     "github.com/IntelliLead/ReviewHandlers/src/pkg/jsonUtil"
     "github.com/IntelliLead/ReviewHandlers/src/pkg/logger"
@@ -12,7 +13,9 @@ import (
     "github.com/aws/aws-sdk-go/aws"
     "github.com/aws/aws-sdk-go/aws/session"
     "github.com/aws/aws-sdk-go/service/dynamodb"
-    "google.golang.org/api/mybusinessaccountmanagement/v1"
+    "golang.org/x/oauth2/google"
+    "io"
+    "net/url"
     "os"
 )
 
@@ -68,32 +71,81 @@ func handleRequest(ctx context.Context, request events.LambdaFunctionURLRequest)
     // --------------------
     // TODO: how to get google user from user ID? Can populate manually, but how to get user to self-serve during onboarding?
     //
-    // client, err := google.DefaultClient(context.TODO(),
-    //     "https://www.googleapis.com/auth/devstorage.full_control")
-    // if err != nil {
-    //     log.Fatal(err)
-    // }
-    // client.Get("...")
-
-    // businessprofileperformanceService, err := businessprofileperformance.NewService(context.Background())
-
-    mybusinessaccountmanagementService, err := mybusinessaccountmanagement.NewService(context.Background())
+    client, err := google.DefaultClient(context.TODO(),
+        "https://www.googleapis.com/auth/business.manage")
     if err != nil {
-        log.Error("Error creating Google business account management service: ", err)
-        return events.LambdaFunctionURLResponse{Body: `{"message": "Error creating Google business account management service"}`, StatusCode: 500}, nil
+        log.Fatal(err)
     }
 
-    resp, err := mybusinessaccountmanagementService.Accounts.List().Do()
+    accountId := "accounts/108579220773034215369" // IL onboarding service account
+    // mucurryAccountId := "accounts/107069853445303760285"
+    // mucurryLocationId := "locations/14282334389737307772"
+    //
+    // jinYuLiangyanAccountId := "accounts/107069853445303760285"
+    // jinYuLiangyanLocationId := "locations/10774539939231103915"
+
+    // listAccountsUrl := "https://mybusinessaccountmanagement.googleapis.com/v1/accounts"
+    listStoresUrl := fmt.Sprintf("https://mybusinessbusinessinformation.googleapis.com/v1/%s/locations", accountId)
+    readMask := "name"
+
+    // Encode the parameters
+    params := url.Values{}
+    params.Set("readMask", readMask)
+
+    // Create the final URL
+    finalURL := listStoresUrl + "?" + params.Encode()
+    // _ = listStoresUrl + "?" + params.Encode()
+
+    resp, err := client.Get(finalURL)
+    // resp, err := client.Get(listAccountsUrl)
     if err != nil {
-        log.Error("Error listing Google business accounts: ", err)
+        log.Error("Error getting Google business accounts: ", err)
+        log.Error("Error details: ", jsonUtil.AnyToJson(err))
 
         return events.LambdaFunctionURLResponse{
-            Body:       `{"message": "Error listing Google business accounts"}`,
+            Body:       `{"message": "Error getting Google business accounts"}`,
             StatusCode: 500,
         }, nil
     }
 
-    log.Info("Successfully listed Google business accounts: ", jsonUtil.AnyToJson(resp))
+    // Read the response body
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        log.Error("Failed to read response body: %s\n", err)
+
+        return events.LambdaFunctionURLResponse{
+            Body:       `{"message": "Failed to read response body"}`,
+            StatusCode: 500,
+        }, nil
+    }
+
+    // Print the response body
+    log.Debug("Response body: ", string(body))
+
+    // // businessprofileperformanceService, err := businessprofileperformance.NewService(context.Background())
+    //
+    // mybusinessaccountmanagementService, err := mybusinessaccountmanagement.NewService(context.Background())
+    // if err != nil {
+    //     log.Error("Error creating Google business account management service: ", err)
+    //     return events.LambdaFunctionURLResponse{Body: `{"message": "Error creating Google business account management service"}`, StatusCode: 500}, nil
+    // }
+    //
+    // // resp, err := mybusinessaccountmanagementService.Accounts.List().Do()
+    // googleReq := mybusinessaccountmanagementService.Accounts.List()
+    // log.Debug("googleReq is ", jsonUtil.AnyToJson(googleReq))
+    // resp, err := googleReq.Do()
+    // if err != nil {
+    //     log.Error("Error listing Google business accounts: ", err)
+    //     log.Error("Error details: ", jsonUtil.AnyToJson(err))
+    //     log.Error("response is ", jsonUtil.AnyToJson(resp))
+    //
+    //     return events.LambdaFunctionURLResponse{
+    //         Body:       `{"message": "Error listing Google business accounts"}`,
+    //         StatusCode: 500,
+    //     }, nil
+    // }
+
+    log.Info("Successfully finished lambda execution")
 
     // --------------------------------
     // forward to LINE by calling LINE messaging API
